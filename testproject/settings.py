013c187cd4b92ab952bf28637b11035641115e6e
azad17 <azadlal.lm8@gmail.com>
2021-11-22 11:36:42 +0530

    static_dir updated

--- testproject/settings.py
+++ testproject/settings.py
@@ -164,9 +164,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[
-    STATIC_DIR
-]
 
 STATIC_ROOT =os.path.join(BASE_DIR,'static/')
 MEDIA_URL = '/media/'
