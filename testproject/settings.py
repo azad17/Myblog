f63baa8cb6d90fade6abf90e6c9586fb17d168b7
azad17 <azadlal.lm8@gmail.com>
2021-11-22 10:42:04 +0530

    static root changed

--- testproject/settings.py
+++ testproject/settings.py
@@ -168,7 +168,7 @@ STATICFILES_DIRS=[
     STATIC_DIR
 ]
 
-STATIC_ROOT =os.path.join(BASE_DIR,'assets')
+STATIC_ROOT =os.path.join(BASE_DIR,'static/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
